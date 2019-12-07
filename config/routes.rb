5c85840106aab660a4518967a88ecb22bd06aded
yasuhirozenhira <poseidon.0141@gmail.com>
2019-12-07 18:17:29 +0900

    retake_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   get '/blogs', to: 'blogs#index'
   get '/stocks', to: 'stocks#index'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
